de7e8781771f1d6480b8efcd49200b4c9a78a710
Rolando Andrade <rolandoandradefernandez@gmail.com>
2024-03-26 08:38:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { useState } from 'react';
+
+function App() {
+
+  const [image, setImage] = useState<string>("")
+  const [name, setName] = useState<string>("")
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImage(event.target.value)
+  }
+
+  return (
+    <main className={'flex flex-row items-center justify-center h-screen'}>
+      <div className="flex flex-col items-center space-y-2">
+        <div className="flex items-center space-x-2">
+          <LockIcon className="h-6 w-6"/>
+          <h2 className="text-2xl font-bold">Identity</h2>
+        </div>
+        <div className="grid w-full max-w-md items-center gap-2">
+          <Label htmlFor="image">URL de la imagen</Label>
+          <Input id="image" placeholder="www.example.com/image.png" onChange={handleImageChange}/>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img
+            alt="Image"
+            className="rounded-md overflow-hidden"
+            height="100"
+            src={image}
+            style={{
+              aspectRatio: "100/100",
+              objectFit: "cover",
+            }}
+            width="100"
+          />
+          <div className="grid gap-0.5">
+            <p className="text-sm font-medium">Image Name</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Enter the URL of the image above to display it here.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+
+  )
+}
+
+function LockIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="11" x="3" y="11" rx="2" ry="2"/>
+      <path d="M7 11V7a5 5 0 0 1 10 0v4"/>
+    </svg>
+  )
+}
+
+export default App
